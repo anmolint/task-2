791e1f1d3518afa483b284d0252070033f5d5d92
rohit <rohit.excel2011@gmail.com>
2022-03-01 09:48:37 +0530

    corrections in returnData

--- controller/returnData.js
+++ controller/returnData.js
@@ -2,14 +2,15 @@ const { default: async } = require("async");
 const data = require('../model/user')
 const dataReturn = async(req,res)=>{
     try{
-        console.log(res.query)
-        const { page = 1, limit = 10 } = req.query;
+        console.log(req.query)
+        const { page = req.query.id, limit = 10 } = req.query;
         const posts = await data.find()
       .limit(limit * 1)
       .skip((page - 1) * limit)
       .exec();
         
       const count = await data.countDocuments();
+      res.send(count,posts)
 res.json({
         posts,
         totalPages: Math.ceil(count / limit),
